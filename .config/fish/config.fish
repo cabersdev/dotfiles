5da871eabe242b9a0ca407b012e449bdc426c4d4
Giovanni <caberlotto.giovanni@gmail.com>
2023-09-26 16:55:01 +0200

    updated

---
+++ .config/fish/config.fish
@@ -0,0 +1,5 @@
+ [ -d /home/linuxbrew/.linuxbrew ] && eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)
+
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
---
+++ .config/hypr/copydots.sh
@@ -0,0 +1,3 @@
+cp -r /home/caber/.config/hypr/ /home/caber/dotfiles/.config/
+cp -r /home/caber/.config/fish/ /home/caber/dotfiles/.config/
+cp -r /home/caber/.config/nvim/ /home/caber/dotfiles/.config/
