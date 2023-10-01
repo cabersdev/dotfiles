a2e21c345a4f1642b591934d543617b1da95a625
Giovanni <caberlotto.giovanni@gmail.com>
2023-10-01 11:34:48 +0200

    Updated dotfiles

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,5 +1,10 @@
  [ -d /home/linuxbrew/.linuxbrew ] && eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)
 
+starship init fish | source 
+
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
+
+set -g fish_greeting
+
